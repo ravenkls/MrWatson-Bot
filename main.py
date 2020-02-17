80346ffaf6cf0358ebe5a5f15d2cddaa7da27600
Kristian <kristian.smith.2001@hotmail.co.uk>
2020-02-17 12:35:17 +0000

    debug

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import psycopg2
 import os
 
+print('STARTING')
 conn = psycopg2.connect(os.environ.get("DATABASE_URL"), sslmode="require")
 cursor = conn.cursor()
-
+print(cursor)
 cursor.execute("CREATE TABLE IF NOT EXISTS reputation_points (member_id INTEGER PRIMARY KEY, points INTEGER);")
-cursor.execute("INSERT INTO reputation_points (member_id, points) VALUES (%s, %s)", (1283619726397126731, 1))
+conn.commit()
+print('TABLE CREATED')
+cursor.execute("INSERT INTO reputation_points (member_id, points) VALUES (%s, %s);", (1283619726397126731, 1))
+print('ENTRY ADDED')
 conn.commit()
