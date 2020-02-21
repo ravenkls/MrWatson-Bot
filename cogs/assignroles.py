14ad278be8de1c76b5db0d40c034ac4cccaec68f
Kristian <kristian.smith.2001@hotmail.co.uk>
2020-02-21 19:51:34 +0000

    Nickname

--- cogs/assignroles.py
+++ cogs/assignroles.py
@@ -34,6 +34,23 @@ class AssignRoles(commands.Cog):
         self.bot.database.remove_role_reaction(message.id, emoji)
         await message.remove_reaction(emoji, ctx.guild.get_member(self.bot.user.id))
     
+    @commands.command(aliases=["nick"])
+    @commands.cooldown(rate=12, per=3600, type=commands.BucketType.user)
+    @commands.guild_only()
+    async def nickname(self, ctx, *, nickname=None):
+        """Change your nickname, to remove your nickname just type `-nickname` on its own."""
+        if nickname is None:
+            year = member.nick.split("||")[-1].strip()
+            if member.name + " || " + year == member.nick:
+                await ctx.send("You already have no nickname! To assign a nickname type `-nickname <nickname>`")
+            else:
+                await member.edit(nick=member.name + " || " + year)
+                await ctx.send("Your nickname has been reset.")
+        else:
+            year = member.nick.split("||")[-1].strip()
+            await member.edit(nick=nickname + " || " + year)
+            await ctx.send("Your nickname has been set!")
+
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
         check = self.bot.database.check_reaction(payload.message_id, payload.emoji)
