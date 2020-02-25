c9cdeccd13f52cc4eab44308e1f4c4ccab0aa3a7
Kristian <kristian.smith.2001@hotmail.co.uk>
2020-02-25 20:15:17 +0000

    records

--- cogs/general.py
+++ cogs/general.py
@@ -277,8 +277,8 @@ class General(commands.Cog):
             channels = await self.bot.database.conn.fetch("SELECT channel_id FROM helper_roles WHERE role_id=$1", matched_roles[0].id)
             for c in channels:
                 other_roles = await self.bot.database.conn.fetch("SELECT role_id FROM helper_roles WHERE channel_id=$1 AND role_id != $2",
-                                                                 c, matched_roles[0].id)
-                matched_roles.extend([ctx.guild.get_role(o) for o in other_roles])
+                                                                 c["channel_id"], matched_roles[0].id)
+                matched_roles.extend([ctx.guild.get_role(o["role_id"]) for o in other_roles])
 
             embed = discord.Embed(title=f"Query for members with role \"{role}\"",
                                   colour=EMBED_ACCENT_COLOUR)
